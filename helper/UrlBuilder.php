a0e3d917c324dcd881d1b54cd56aa50dd9140009
wangzhishou <wangzhishou@qq.com>
2014-01-09 16:34:29 +0800

    基础函数修改
    基础函数修改

--- helper/UrlBuilder.php
+++ helper/UrlBuilder.php
@@ -14,66 +14,80 @@
  * @since 1.1
  */
 class UrlBuilder {
-
-    /**
-     * Build URL based on a route Id.
-     * @param string $id Id of the route
-     * @param array $param Parameter values to build the route URL
-     * @param bool $addAppUrl Add the APP_URL to the url
-     * @return string URL of a route
-     */
-    public static function url($id, $param=null, $addAppUrl=false){
-        $route = Q::app()->route;
-        $routename = null;
-        foreach($route as $req=>$r){
-            foreach($r as $rname=>$value){
-                if(isset($value['id']) && $value['id']==$id){
-                    $routename = $rname;
-                    break;
-                }
-            }
-        }
-
-        if($addAppUrl)
-            $routename = Q::conf()->APP_URL . substr($routename,1);
-
-        if($param!=null){
-            foreach($param as $k=>$v){
-                $routename = str_replace(':' . $k, $v, $routename);
-            }
-        }
-
-        return $routename;
-    }
-
-    /**
-     * Build URL based on a route Controller and Method.
-     * @param string $controller Name of the Controller
-     * @param string $method Name of the Action method
-     * @param array $param Parameter values to build the route URL
-     * @param bool $addAppUrl Add the APP_URL to the url
-     * @return string URL of a route
-     */
-    public static function url2($controller, $method, $param=null, $addAppUrl=false){
-        $route = Q::app()->route;
-        $routename = null;
-        foreach($route as $req=>$r){
-            foreach($r as $rname=>$value){
-                if($value[0]==$controller && $value[1]==$method){
-                    $routename = $rname;
-                    break;
-                }
-            }
-        }
-        if($addAppUrl)
-            $routename = Q::conf()->APP_URL . substr($routename,1);
-
-        if($param!=null){
-            foreach($param as $k=>$v){
-                $routename = str_replace(':' . $k, $v, $routename);
-            }
-        }
-
-        return $routename;
-    }
+	
+	/**
+	 * Build URL based on a route Id.
+	 *
+	 * @param string $id
+	 *        	Id of the route
+	 * @param array $param
+	 *        	Parameter values to build the route URL
+	 * @param bool $addAppUrl
+	 *        	Add the APP_URL to the url
+	 * @return string URL of a route
+	 */
+	public static function url($id, $param = null, $addAppUrl = false) {
+		$route = Q::app ()->route;
+		$routename = null;
+		foreach ( $route as $req => $r ) {
+			foreach ( $r as $rname => $value ) {
+				if (isset ( $value ['id'] ) && $value ['id'] == $id) {
+					$routename = $rname;
+					break;
+				}
+			}
+		}
+		
+		if ($addAppUrl)
+			$routename = Q::conf ()->APP_URL . substr ( $routename, 1 );
+		
+		if ($param != null) {
+			foreach ( $param as $k => $v ) {
+				$routename = str_replace ( ':' . $k, $v, $routename );
+			}
+		}
+		
+		return $routename;
+	}
+	
+	/**
+	 * Build URL based on a route Controller and Method.
+	 *
+	 * @param string $controller
+	 *        	Name of the Controller
+	 * @param string $method
+	 *        	Name of the Action method
+	 * @param array $param
+	 *        	Parameter values to build the route URL
+	 * @param bool $addAppUrl
+	 *        	Add the APP_URL to the url
+	 * @return string URL of a route
+	 */
+	public static function url2($controller, $method, $param = null, $addAppUrl = false) {
+		$route = Q::app ()->route;
+		$routename = null;
+		foreach ( $route as $req => $r ) {
+			foreach ( $r as $rname => $value ) {
+				if ($value [0] == $controller && $value [1] == $method) {
+					if (isset ( $value ["extension"] )) {
+						$routename = $rname . $value ["extension"] [0];
+					} else {
+						$routename = $rname;
+					}
+					break;
+				}
+			}
+		}
+		if ($addAppUrl) {
+			$routename = Q::conf ()->APP_URL . substr ( $routename, 1 );
+		}
+		
+		if ($param != null) {
+			foreach ( $param as $k => $v ) {
+				$routename = str_replace ( ':' . $k, $v, $routename );
+			}
+		}
+		
+		return $routename;
+	}
 }
